c01abfbbba4860ec62ad04967e4b18711a2e6541
Alex Hokanson <alex.hokanson@docker.com>
2019-01-31 09:45:12 -0500

    new accumulator movement working

--- main/snake/go_snake_body.go
+++ main/snake/go_snake_body.go
@@ -1,3 +1,18 @@
+components {
+  id: "controller"
+  component: "/main/snake/scr_snake_body.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
